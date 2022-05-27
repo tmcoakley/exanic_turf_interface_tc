a2582c8b5248ef144580ebbf67e309b06f106eb1
tmcoakley <103058251+tmcoakley@users.noreply.github.com>
2022-05-27 16:24:20 -0400

    Update testexample.py

--- Test_Code_to_Interface_with_TURF/testexample.py
+++ Test_Code_to_Interface_with_TURF/testexample.py
@@ -1,16 +1,10 @@
-import sfc_interface_udp
-import turf_data_intepreter
+from sfc_interface_udp import packet
+from turf_data_intepreter import packetparser as ppar
 
-msg = b"\x00\x00\x00\x01"
-print("Read address 0")
-read_packet = sfc_interface_udp.packet(
-    msg
-)
-read_packet.print_ack()
+#packet = packet()
+#packet.write(b"\x10\x55\x00\x01\x10\x00\x00\x01")
+#packet.print_ack()
 
-addr = read_packet.addr28bitrecd
-tag = read_packet.tag4bitrecd
-data = read_packet.data32bitrecd
-print(hex(data))
-
-interp_packet = turf_data_intepreter.packetparser (data, tag, addr)
+packet.read(b"\x10\x00\x00\x00")
+ppar(packet.datarecd, packet.tagrecd, packet.addrrecd)
+packet.print_ack()
