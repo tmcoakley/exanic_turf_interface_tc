46df41ec49dade5dfc18f36446bc069974b97a5e
tmcoakley <103058251+tmcoakley@users.noreply.github.com>
2022-06-27 15:16:59 -0400

    Update testexample.py

--- Test_Code_to_Interface_with_TURF/testexample.py
+++ Test_Code_to_Interface_with_TURF/testexample.py
@@ -1,17 +1,22 @@
-from sfc_interface_udp import packet
-from turf_data_intepreter import packetparser as ppar
+from common.sfc_interface_udp import packet
+from common.turf_data_interpreter import packetparser as ppar
 
-packet = packet()
+# INTERFACE IP AND PORT
+MY_IP = "127.0.0.1"
+MY_PORT = 21347
+
+
+packet = packet(MY_IP, MY_PORT)
 #packet.write(b"\x10\x55\x00\x01\x10\x00\x00\x01")
 #packet.print_ack()
 
-bytesReturn = packet.read(b"\x10\x00\x00\x00")
+bytesReturn = packet.read(b"\x20\x00\x00\x01")
 print(bytesReturn)
 ppar(packet.datarecd, packet.tagrecd, packet.addrrecd)
+print('NEW', packet.newtag)
 packet.print_ack()
 #print()
-bytesReturn = packet.read(b"\x10\x00\x00\x01")
-print(bytesReturn.hex('x'))
+# bytesReturn = packet.read(b"\x10\x00\x00\x01")
+# print(bytesReturn.hex('x'))
 #ppar(packet.datarecd, packet.tagrecd, packet.addrrecd)
 #packet.print_ack()
-
