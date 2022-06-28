5acdfefb903aaf2f42ea6234dd856a412093d39e
tmcoakley <103058251+tmcoakley@users.noreply.github.com>
2022-06-28 15:46:12 -0400

    Update testexample.py

--- Test_Code_to_Interface_with_TURF/testexample.py
+++ Test_Code_to_Interface_with_TURF/testexample.py
@@ -1,12 +1,12 @@
-from common.sfc_interface_udp import packet
-from common.turf_data_interpreter import packetparser as ppar
+from sfc_interface_udp import Packet
+from turf_data_intepreter import packetparser as ppar
 
 # INTERFACE IP AND PORT
 MY_IP = "127.0.0.1"
 MY_PORT = 21347
 
 
-packet = packet(MY_IP, MY_PORT)
+packet = Packet(MY_IP, MY_PORT)
 #packet.write(b"\x10\x55\x00\x01\x10\x00\x00\x01")
 #packet.print_ack()
 
